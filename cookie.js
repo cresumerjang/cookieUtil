b8ad21aaeaf5be01f4101ad7f1774230df7e4b9b
JANGJAEWON <jw9651@tmon.co.kr>
2015-08-28 12:49:31 +0900

    implement each method

--- cookie.js
+++ cookie.js
@@ -1,17 +1,48 @@
 var htCookie = {
+    /**
+     * [브라우저 쿠키 지원여부]
+     * @return {[Boolean]}
+     */
     bSupport : function(){
-
+        return navigator.cookieEnabled;
     },
 
-    set : function( name, value, htOpt ){
+    set : function( sName, sValue, htOpt ){
+        var aCookie = [];
+        var options = htOpt || {};
+        var htCookieData = {
+            name : encodeURIComponent( sName ) + "=" + encodeURIComponent( sValue );
+            path : options.path;
+            domain : options.domain;
+            expires : options.expires;
+        };
 
+        // TODO expires date 설정
+
+        for( v in htCookieDate ){
+            if( htCookieDate[v] != undefined ){
+                aCookie.push( htCookieDate[v] );
+            }
+        }
+
+        document.cookie = aCookie.join[';'];
     },
 
-    get : function( name ){
+    get : function( sName ){
+        if( this.bSupport() === false || document.cookie === '' ){
+            return;
+        }
 
+        var sCookie = document.cookie.split(';');
+
+        for( var i = 0, j = sCookie.length; i < j; i++ ){
+            if( sCookie[i].split('=')[0] === sName ){
+                return sCookie[i].split('=')[1];
+            }
+        }
     },
 
-    delete : function( name ){
-
+    delete : function( sName ){
+        this.set( sName, '', { expires : -1 } );
     }
 };
