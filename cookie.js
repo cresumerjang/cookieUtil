206fc962c6d1f61956fe6e045045f615c9e3ddb1
JANG JAE WON <cresumerjang@gmail.com>
2015-08-28 11:55:57 +0900

    [init] cookie module

---
+++ cookie.js
@@ -0,0 +1,17 @@
+var htCookie = {
+    bSupport : function(){
+
+    },
+
+    set : function( name, value, htOpt ){
+
+    },
+
+    get : function( name ){
+
+    },
+
+    delete : function( name ){
+
+    }
+};
